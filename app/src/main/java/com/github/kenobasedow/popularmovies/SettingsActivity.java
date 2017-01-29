b7c3a26bc9123522bca30ce7e27a28919f94f25f
Pride Haveit <pride.haveit@gmail.com>
2017-01-29 16:54:41 +0100

    Using layout file to start settings fragment.

--- app/src/main/java/com/github/kenobasedow/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/github/kenobasedow/popularmovies/SettingsActivity.java
@@ -8,10 +8,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
+        setContentView(R.layout.activity_settings);
     }
 
 }
