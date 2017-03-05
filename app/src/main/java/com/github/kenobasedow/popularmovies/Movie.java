5491d7e201ea47458ddd74758f2b010f6d5276d8
Pride Haveit <pride.haveit@gmail.com>
2017-03-05 10:22:37 +0100

    Using data binding for detail activity.

--- app/src/main/java/com/github/kenobasedow/popularmovies/Movie.java
+++ app/src/main/java/com/github/kenobasedow/popularmovies/Movie.java
@@ -1,9 +1,16 @@
 package com.github.kenobasedow.popularmovies;
 
+import android.databinding.BindingAdapter;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.widget.ImageView;
+
+import com.github.kenobasedow.popularmovies.utilities.NetworkUtils;
+import com.squareup.picasso.Picasso;
 
 public class Movie implements Parcelable {
+
+    private static final String FORMAT_RATING = "%.1f/10";
 
     public static final String TAG = Movie.class.getName();
 
@@ -14,6 +21,23 @@ public class Movie implements Parcelable {
     public String releaseDate;
 
     public Movie() {
+    }
+
+    public String getRatingString() {
+        return String.format(FORMAT_RATING, rating);
+    }
+
+    public String getPictureUrl() {
+        return NetworkUtils.MOVIE_PICTURE_BASE_URL + picturePath;
+    }
+
+    @BindingAdapter({"bind:imageUrl"})
+    public static void loadImage(ImageView imageView, String imageUrl) {
+        Picasso.with(imageView.getContext())
+                .load(imageUrl)
+                .placeholder(R.mipmap.ic_launcher)
+                .error(R.mipmap.ic_launcher)
+                .into(imageView);
     }
 
     public Movie(Parcel parcel) {
--- app/src/main/java/com/github/kenobasedow/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/com/github/kenobasedow/popularmovies/MovieDetailActivity.java
@@ -1,49 +1,21 @@
 package com.github.kenobasedow.popularmovies;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.github.kenobasedow.popularmovies.utilities.NetworkUtils;
-import com.squareup.picasso.Picasso;
+import com.github.kenobasedow.popularmovies.databinding.ActivityMovieDetailBinding;
 
 public class MovieDetailActivity extends AppCompatActivity {
-
-    private static final String FORMAT_RATING = "%.1f/10";
-
-    TextView mMovieTitleTextView;
-    TextView mMovieReleaseDateTextView;
-    TextView mMovieRatingTextView;
-    TextView mMoviePlotTextView;
-
-    ImageView mMoviePictureImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_detail);
-
-        mMovieTitleTextView = (TextView) findViewById(R.id.tv_movie_title);
-        mMovieReleaseDateTextView = (TextView) findViewById(R.id.tv_movie_release_date);
-        mMovieRatingTextView = (TextView) findViewById(R.id.tv_movie_rating);
-        mMoviePlotTextView = (TextView) findViewById(R.id.tv_movie_plot);
-
-        mMoviePictureImageView = (ImageView) findViewById(R.id.iv_movie_picture);
+        ActivityMovieDetailBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_movie_detail);
 
         if (getIntent().hasExtra(Movie.TAG)) {
             Movie movie = (Movie) getIntent().getParcelableExtra(Movie.TAG);
-
-            mMovieTitleTextView.setText(movie.title);
-            mMovieReleaseDateTextView.setText(movie.releaseDate);
-            mMovieRatingTextView.setText(String.format(FORMAT_RATING, movie.rating));
-            mMoviePlotTextView.setText(movie.plot);
-
-            Picasso.with(this)
-                    .load(NetworkUtils.MOVIE_PICTURE_BASE_URL + movie.picturePath)
-                    .placeholder(R.mipmap.ic_launcher)
-                    .error(R.mipmap.ic_launcher)
-                    .into(mMoviePictureImageView);
+            binding.setMovie(movie);
         }
     }
 }
